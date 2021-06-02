77c728d25201716c9d7a6e41417a01f389fc1370
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-02 15:51:10 +0300

    init database

--- blog/app.rb
+++ blog/app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'bloginsinatra.db'
+	@db.results_as_hash = true 
+end
+
+before do
+	init_db   # индициализация БД
+end
 
 get '/' do
 	erb "Hello!"
