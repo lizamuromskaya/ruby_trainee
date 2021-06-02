d3d1998a0a325aafdf8439cdd7e6b6cbfd7df6c1
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-02 16:21:37 +0300

    Saved content in database

--- blog/app.rb
+++ blog/app.rb
@@ -38,5 +38,6 @@ post '/new' do
 		@error='Enter your post!'
 		return erb  :new
 	end
+	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 	erb "You typed #{content}"
 end
