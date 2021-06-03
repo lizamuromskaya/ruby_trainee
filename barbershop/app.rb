6ee05040d12867a379ba62ec8001f370da45bdb2
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-03 08:42:48 +0300

    added saving users information

--- barbershop/app.rb
+++ barbershop/app.rb
@@ -7,16 +7,18 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
-
 end
 
 before do
 	@barbers = Barber.all
 end 
-
 
 get '/' do
 	@barbers=Barber.all
@@ -25,6 +27,7 @@ end
 
 get '/visit' do
 	@c = Client.new
+
   erb :visit
 end
 
@@ -34,7 +37,7 @@ post '/visit' do
 	if @c.save
 		erb "<h2>Thank you!</h2>"
 	else
-		#@error = @c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
