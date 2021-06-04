f311020bcba48759e385c103743e2e33dcc67d06
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-04 21:12:58 +0300

    made cart with number of items

--- pizzashop/public/app.js
+++ pizzashop/public/app.js
@@ -6,6 +6,7 @@ function add_to_cart(id){
 	x=x*1+1;
 	window.localStorage.setItem(key,x);
 	update_orders_input();
+	update_orders_button();
 }
 
 function cart_get_number_of_items(){
@@ -29,6 +30,11 @@ function update_orders_input(){
 	var orders =cart_get_orders();
 	$('#orders_input').val(orders);
 }
+function update_orders_button(){
+
+	var text = 'Cart (' + cart_get_number_of_items()+')';
+	$('#orders_button').val(text);
+}
 
 function cart_get_orders(){
 	var orders = '';
@@ -46,4 +52,5 @@ function cart_get_orders(){
 
 	return orders;
 }
+
 
