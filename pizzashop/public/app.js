365d7747480c5231628d28ce4aa7c9820d6b7402
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-04 21:01:39 +0300

    update orders input field

--- pizzashop/public/app.js
+++ pizzashop/public/app.js
@@ -1,15 +1,10 @@
-function something(){
-	var x=window.localStorage.getItem('aaa');// x=hh['aaa']
-	x=x*1+1;
-	window.localStorage.setItem('aaa',x); //hh['aaa']=x
-	alert(x);
-}
 
 function add_to_cart(id){
 	var key='product_'+id;
 	x=window.localStorage.getItem(key);
 	x=x*1+1;
 	window.localStorage.setItem(key,x);
+	update_orders_input();
 }
 
 function cart_get_number_of_items(){
@@ -17,15 +12,39 @@ function cart_get_number_of_items(){
 
 	for(var i = 0; i < window.localStorage.length; i++)
 	{
-		var key = window.localStorage.key(i); // получаем ключ
-		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
-
-		if(key.indexOf('product_') == 0)
+		var key = window.localStorage.key(i); // getting key
+		var value = window.localStorage.getItem(key); //hh[key] = x
+		if(key.indexOf('product_') == 0) //if key started with product_
 		{
 			cnt = cnt + value * 1;
 		}
 	}
-
 	return cnt;
+}
+
+
+function update_orders_input(){
+
+	var orders =cart_get_orders();
+	$('#orders_input').val(orders);
+
+
+}
+
+function cart_get_orders(){
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // getting key
+		var value = window.localStorage.getItem(key); //hh[key] = x
+
+		if(key.indexOf('product_') == 0) //if key started with product_
+		{
+			orders=orders+key+'='+value+',';
+		}
+	}
+
+	return orders;
 }
 
