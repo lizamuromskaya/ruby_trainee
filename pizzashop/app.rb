fb1bc70354dede1c9bb7dd02025d655712b1bb91
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-04 11:28:23 +0300

    added rekefile, ipdate gemfile,added modele

--- pizzashop/app.rb
+++ pizzashop/app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#set :bind, '0.0.0.0'
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
+
+class Product < ActiveRecord::Base
+	#validates :name, presence: true
+end
 
 get '/' do
 	erb :index
