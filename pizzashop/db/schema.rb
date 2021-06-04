28b5a55762afd218308b6b206c1c4d640a9abac3
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-06-04 12:46:17 +0300

    Added 3 pizzas

--- pizzashop/db/schema.rb
+++ pizzashop/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_082940) do
+ActiveRecord::Schema.define(version: 2021_06_04_093552) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
